26ece45ef97dbc5f6d916ede4318a02af4f8cd30
avril975 <avrilubrique@gmail.com>
2026-02-26 09:07:18 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+// 1. Seleccionamos el título
+const titulo = document.querySelector("h1");
 
+// 2. Cambiamos su texto
+titulo.textContent = "Tiendas";
+
+// 3. Cambiamos su color de fondo
+titulo.style.backgroundColor = "pink";
