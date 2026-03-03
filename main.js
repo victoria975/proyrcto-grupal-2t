2927b9efd26b3a7791fbaea3fe04b34836ede663
carolina0305 <carolinagarciaroman1@gmail.com>
2026-03-03 09:12:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,19 @@ titulo.textContent = "Tiendas";
 
 // 3. Cambiamos su color de fondo
 titulo.style.backgroundColor = "pink";
+
+// 1. Seleccionamos el botón
+const miBoton = document.querySelector("#boton-magico");
+
+// 2. Le pedimos que escuche el "click"
+miBoton.addEventListener("click", function() {
+    console.log("¡Has pulsado el botón!");
+    document.body.style.backgroundColor = "lightblue";
+});
+
+const boton = document.querySelector("#btn-modo");
+
+boton.addEventListener("click", function() {
+    // Alterna la clase 'noche' en todo el cuerpo de la web
+    document.body.classList.toggle("noche");
+});
